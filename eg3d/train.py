0f4616329fe75294fddec232c672cb682be4c43e
zhuoqian <yzhq97@gmail.com>
2022-11-04 13:13:30 +0800

    train on shhq

--- eg3d/train.py
+++ eg3d/train.py
@@ -148,8 +148,8 @@ def parse_comma_separated_list(s):
 @click.option('--p',            help='Probability for --aug=fixed', metavar='FLOAT',            type=click.FloatRange(min=0, max=1), default=0.2, show_default=True)
 @click.option('--target',       help='Target value for --aug=ada', metavar='FLOAT',             type=click.FloatRange(min=0, max=1), default=0.6, show_default=True)
 @click.option('--batch-gpu',    help='Limit batch size per GPU', metavar='INT',                 type=click.IntRange(min=1))
-@click.option('--cbase',        help='Capacity multiplier', metavar='INT',                      type=click.IntRange(min=1), default=32768, show_default=True)
-@click.option('--cmax',         help='Max. feature maps', metavar='INT',                        type=click.IntRange(min=1), default=512, show_default=True)
+@click.option('--cbase',        help='Capacity multiplier', metavar='INT',                      type=click.IntRange(min=1), default=24576, show_default=True)
+@click.option('--cmax',         help='Max. feature maps', metavar='INT',                        type=click.IntRange(min=1), default=384, show_default=True)
 @click.option('--glr',          help='G learning rate  [default: varies]', metavar='FLOAT',     type=click.FloatRange(min=0))
 @click.option('--dlr',          help='D learning rate', metavar='FLOAT',                        type=click.FloatRange(min=0), default=0.002, show_default=True)
 @click.option('--map-depth',    help='Mapping network depth  [default: varies]', metavar='INT', type=click.IntRange(min=1), default=2, show_default=True)
@@ -311,8 +311,8 @@ def main(**kwargs):
         })
     elif opts.cfg == 'shhq':
         rendering_options.update({
-            'depth_resolution': 64,
-            'depth_resolution_importance': 64,
+            'depth_resolution': 48,
+            'depth_resolution_importance': 48,
             'ray_start': 'auto',
             'ray_end': 'auto',
             'box_warp': 1,
